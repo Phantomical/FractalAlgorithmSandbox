77087df3e4aa871ccdf4c0c12bd2cf0a18eed33a
Sean889 <sean@lynches.ca>
2015-09-06 22:28:50 -0400

    Removed unused usings

--- Window.cs
+++ Window.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenTK;
+﻿using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using ShaderRuntime;
-using System.Drawing;
+using System;
 
 namespace FractalAlgorithmTest
 {
