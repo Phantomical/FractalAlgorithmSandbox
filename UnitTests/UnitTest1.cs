b9bd9c9bac9bc813e595a5c8200ff94f12ae13e4
Phantomical <phantom@lynches.ca>
2015-10-26 16:39:45 -0400

    Moved unit tests to within project folder

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FractalAlgorithmTest;
+
+namespace UnitTests
+{
+	[Serializable]
+	public class DummyException : Exception
+	{
+		public DummyException() { }
+		public DummyException(string message) : base(message) { }
+		public DummyException(string message, Exception inner) : base(message, inner) { }
+		protected DummyException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context)
+			: base(info, context) { }
+	}
+
+	[TestClass]
+	public class UnitTest
+	{
+		static int Counter = 0;
+
+		static float CounterMethod(float x, float y, float z)
+		{
+			Counter++;
+			return 0;
+		}
+		static float ThrowMethod(float x, float y, float z)
+		{
+			throw new DummyException();
+		}
+
+		[TestMethod]
+		public void PlaneCallsMethod()
+		{
+			try
+			{
+				new Plane(32f, 32, ThrowMethod);
+			}
+			catch(DummyException)
+			{
+				//Test Succeeded
+				Assert.IsTrue(true);
+				return;
+			}
+
+			//The method was not called
+			Assert.Fail("Plane did not call the method");
+		}
+
+		[TestMethod]
+		public void TestPlane()
+		{
+			const int vpside = 32;
+
+			new Plane(1f, vpside, CounterMethod);
+
+			Assert.AreEqual(Counter, vpside * vpside, "Noise method not called for all vertices.");
+
+			Counter = 0;
+		}
+	}
+}
